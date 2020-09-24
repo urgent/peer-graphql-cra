595664a505c02f027abd549fc2fbcad4942e937b
Steve Hartken <steve.hartken@gmail.com>
2020-09-24 00:08:12 -0400

    #14 -- DocBloc

--- src/features/peer-graphql/security/peer.tsx
+++ src/features/peer-graphql/security/peer.tsx
@@ -7,18 +7,22 @@ import { TaskEither } from 'fp-ts/lib/TaskEither'
 import { IOEither } from 'fp-ts/lib/IOEither'
 
 /**
- * Extensible map of message to function
+ * Extensible map of peer messages to a reducer function
  */
 export class Reducer {}
 
-type Reduction = E.Either<
+/**
+ * Enforces return value from reducer to be callable
+ */
+export type Reduction = E.Either<
   Error,
   [TaskEither<Error, Promise<void>> | IOEither<Error, void>, ST]
 >
 
 /**
  * Respond to WebSocket payload with applicable reducer
- * @param evt WebSocket payload
+ * @param {MessageEvent} evt WebSocket payload
+ * @return {Reduction} Error or task of reducer
  */
 export const reduce = (evt: MessageEvent): Reduction =>
   pipe(
@@ -31,11 +35,22 @@ export const reduce = (evt: MessageEvent): Reduction =>
     E.map((state: ST) => [Reducer.prototype[state.message](state), state])
   )
 
+/**
+ * Run side-effects returned by reducer
+ * @param {MessageEvent} evt WebSocket payload
+ * @return {Reduction} Error or results of reducer call
+ */
 export const relay = flow(
   reduce,
   E.map(([reduction]) => reduction())
 )
 
+/**
+ * Run codec on unknown value and make errors printable
+ * @param {t.Decoder<unknown, A>} codec
+ * @param {unknown} i payload to decode
+ * @return {E.Either<Error, A>} decode results, Error or type
+ */
 export function decode<A> (codec: t.Decoder<unknown, A>) {
   return flow(
     codec.decode,
@@ -43,6 +58,11 @@ export function decode<A> (codec: t.Decoder<unknown, A>) {
   )
 }
 
+/**
+ * SHA-1 hash for string to unique token
+ * @param {string} message generic string to hash
+ * @returns {string} SHA-1 hash
+ */
 export async function digestMessage (message: string) {
   const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
   const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8) // hash the message
